79990d9705af5af5d2585b48456815c8ca8d70f9
Andy Zelenak <andyz@utexas.edu>
2016-11-10 10:32:24 -0600

    Rolling back some previous changes to CMakeLists that didn't help with the Travis build. COpying the troublesome header file (ui_mainwindow.h) to the cpp file.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -27,7 +27,76 @@
 ****************************************************************************/
 
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+
+
+//////////////////////////////////////////////////
+// Pasting the contents of ui_mainwindow.h here,
+// since it was causing Travis build failues
+//////////////////////////////////////////////////
+#include <QMessageBox>
+#include <QtGui>
+#include <QtCore>
+#include <QString>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QGridLayout *gridLayout;
+    QHBoxLayout *horizontalLayout;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString("MainWindow"));
+        MainWindow->resize(400, 300);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QString("centralWidget"));
+        gridLayout = new QGridLayout(centralWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QString("gridLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QString("horizontalLayout"));
+
+        gridLayout->addLayout(horizontalLayout, 0, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QString("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 400, 20));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QString("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QString("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
 
 MainWindow::MainWindow(QWidget *parent, ros::NodeHandle* nh) :
     QMainWindow(parent),
