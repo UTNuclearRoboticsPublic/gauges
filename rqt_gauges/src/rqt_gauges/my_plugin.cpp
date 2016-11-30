759d681b58c579ec745747fd4d17e2207d2fad31
Andy Zelenak <andyz@utexas.edu>
2016-11-30 10:56:54 -0600

    Just need to spin and take command line arguments.

--- rqt_gauges/src/rqt_gauges/my_plugin.cpp
+++ rqt_gauges/src/rqt_gauges/my_plugin.cpp
@@ -1,17 +1,22 @@
 
 #include <pluginlib/class_list_macros.h>
 #include <QStringList>
-#include "rqt_gauges/my_plugin.h"
-#include "rqt_gauges/qcgaugewidget.h"
+#include <rqt_gauges/my_plugin.h>
+#include <rqt_gauges/qcgaugewidget.h>
 
 namespace rqt_gauges {
 
+// Callback to change the position of the needle
+void MyPlugin::newDataCallback(const std_msgs::Float64& msg)
+{
+  mSpeedNeedle_-> setCurrentValue(msg.data);
+}
+
+// Constructor is called first before initPlugin function, needless to say.
 MyPlugin::MyPlugin()
   : rqt_gui_cpp::Plugin()
   , widget_(0)
 {
-  // Constructor is called first before initPlugin function, needless to say.
-
   // give QObjects reasonable names
   setObjectName("MyPlugin");
 }
@@ -22,21 +27,62 @@ void MyPlugin::initPlugin(qt_gui_cpp::PluginContext& context)
   QStringList argv = context.argv();
   // create QWidget
   mSpeedGauge_ = new QcGaugeWidget();
-  //widget_ = new QWidget();
-  widget_ = mSpeedGauge_;
+  widget_ = mSpeedGauge_; // This works because mSpeedGauge is inherited from a QWidget class, I guess
   // extend the widget with all attributes and children from UI file
   ui_.setupUi(widget_);
 
   // Set up the gauge
+  const int gaugeSize = 150;
+  mSpeedGauge_->setFixedHeight(gaugeSize);
+  mSpeedGauge_->setFixedWidth(gaugeSize);
   mSpeedGauge_->addBackground(99);
+  QcBackgroundItem *bkg1 = mSpeedGauge_->addBackground(92);
+  bkg1->clearrColors();
+  bkg1->addColor(0.1,Qt::black);
+  bkg1->addColor(1.0,Qt::white);
+
+  QcBackgroundItem *bkg2 = mSpeedGauge_->addBackground(88);
+  bkg2->clearrColors();
+  bkg2->addColor(0.1,Qt::gray);
+  bkg2->addColor(1.0,Qt::darkGray);
+
+  // Range of the indicator
+  int minimum = 0;
+  //nh->getParam("minimum", minimum);
+  int maximum = 100;
+  //nh->getParam("maximum", maximum);
+  //mSpeedGauge->addArc(55);
+  mSpeedGauge_->addDegrees(65)->setValueRange(minimum,maximum);
+  //mSpeedGauge->addColorBand(50);
+  mSpeedGauge_->addValues(80)->setValueRange(minimum,maximum);
+
+  // Gauge label
+  std::string gaugeName = "Roll";
+  //nh->getParam("gauge_name", gaugeName);
+  mSpeedGauge_->addLabel(70)->setText(gaugeName.c_str());
+
+  // The needle
+  QcLabelItem *lab = mSpeedGauge_->addLabel(40);
+  lab->setText("0");
+  mSpeedNeedle_ = mSpeedGauge_->addNeedle(60);
+  mSpeedNeedle_->setLabel(lab);
+  mSpeedNeedle_->setColor(Qt::black);
+  mSpeedNeedle_->setValueRange(minimum,maximum);
+  mSpeedGauge_->addBackground(7);
+  mSpeedGauge_->addGlass(88);
 
   // add widget to the user interface
   context.addWidget(widget_);
+
+  //ros::NodeHandle nh;
+  //rqt_gauges::nhPtr = &nh;
+  needleSub_ = getNodeHandle().subscribe ("roll", 1, &rqt_gauges::MyPlugin::newDataCallback, this);
+  //rqt_gauges::needleSubPtr = &rqt_gauges::needleSub;
 }
 
 void MyPlugin::shutdownPlugin()
 {
-  // TODO unregister all publishers here
+  needleSub_.shutdown();
 }
 
 void MyPlugin::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
